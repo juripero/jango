c36aecee3c730f22ea0be0b1520e73640917fa9a
juripero <wzhou12345@gmail.com>
2025-03-31 17:19:12 -0700

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,16 @@
+if [[ $1 == "1" ]]; then
+   curl -d $2 -X POST http://192.168.56.10:8080/users/Remove/
+fi
+
+if [[ $1 == "2" ]]; then
+   curl -d $2 -X POST http://192.168.56.10:8080/users/Remove/
+fi
+
+if [[ $1 == "3" ]]; then
+   curl -d $2 -X POST http://192.168.56.10:8080/users/Remove/
+fi
+
+if [[ $1 == "4" ]]; then
+   curl -d $2 -X POST http://192.168.56.10:8080/users/Remove/
+fi
+
